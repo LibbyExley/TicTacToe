9eabd6e35394e29951aef209efe5ef91682d67cb
Libby <libby.exley@gmail.com>
2019-10-22 22:07:55 +0100

    initial commit

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace TicTacToe
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Game));
+            this.A1 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.playerBox = new System.Windows.Forms.GroupBox();
+            this.OLabel = new System.Windows.Forms.Label();
+            this.XLabel = new System.Windows.Forms.Label();
+            this.isLabel2 = new System.Windows.Forms.Label();
+            this.isLabel1 = new System.Windows.Forms.Label();
+            this.player2NameLabel = new System.Windows.Forms.Label();
+            this.player1NameLabel = new System.Windows.Forms.Label();
+            this.player2Name = new System.Windows.Forms.TextBox();
+            this.player1Name = new System.Windows.Forms.TextBox();
+            this.playButton = new System.Windows.Forms.Button();
+            this.playerBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // A1
+            // 
+            this.A1.BackColor = System.Drawing.Color.RoyalBlue;
+            resources.ApplyResources(this.A1, "A1");
+            this.A1.Name = "A1";
+            this.A1.UseMnemonic = false;
+            this.A1.UseVisualStyleBackColor = false;
+            this.A1.Click += new System.EventHandler(this.SelectTile);
+            // 
+            // B1
+            // 
+            this.B1.BackColor = System.Drawing.Color.RoyalBlue;
+            resources.ApplyResources(this.B1, "B1");
+            this.B1.Name = "B1";
+            this.B1.UseMnemonic = false;
+            this.B1.UseVisualStyleBackColor = false;
+            this.B1.Click += new System.EventHandler(this.SelectTile);
+            // 
+            // C1
+            // 
+            this.C1.BackColor = System.Drawing.Color.RoyalBlue;
+            resources.ApplyResources(this.C1, "C1");
+            this.C1.Name = "C1";
+            this.C1.UseMnemonic = false;
+            this.C1.UseVisualStyleBackColor = false;
+            this.C1.Click += new System.EventHandler(this.SelectTile);
+            // 
+            // A2
+            // 
+            this.A2.BackColor = System.Drawing.Color.RoyalBlue;
+            resources.ApplyResources(this.A2, "A2");
+            this.A2.Name = "A2";
+            this.A2.UseMnemonic = false;
+            this.A2.UseVisualStyleBackColor = false;
+            this.A2.Click += new System.EventHandler(this.SelectTile);
+            // 
+            // B2
+            // 
+            this.B2.BackColor = System.Drawing.Color.RoyalBlue;
+            resources.ApplyResources(this.B2, "B2");
+            this.B2.Name = "B2";
+            this.B2.UseMnemonic = false;
+            this.B2.UseVisualStyleBackColor = false;
+            this.B2.Click += new System.EventHandler(this.SelectTile);
+            // 
+            // C2
+            // 
+            this.C2.BackColor = System.Drawing.Color.RoyalBlue;
+            resources.ApplyResources(this.C2, "C2");
+            this.C2.Name = "C2";
+            this.C2.UseMnemonic = false;
+            this.C2.UseVisualStyleBackColor = false;
+            this.C2.Click += new System.EventHandler(this.SelectTile);
+            // 
+            // A3
+            // 
+            this.A3.BackColor = System.Drawing.Color.RoyalBlue;
+            resources.ApplyResources(this.A3, "A3");
+            this.A3.Name = "A3";
+            this.A3.UseMnemonic = false;
+            this.A3.UseVisualStyleBackColor = false;
+            this.A3.Click += new System.EventHandler(this.SelectTile);
+            // 
+            // B3
+            // 
+            this.B3.BackColor = System.Drawing.Color.RoyalBlue;
+            resources.ApplyResources(this.B3, "B3");
+            this.B3.Name = "B3";
+            this.B3.UseMnemonic = false;
+            this.B3.UseVisualStyleBackColor = false;
+            this.B3.Click += new System.EventHandler(this.SelectTile);
+            // 
+            // C3
+            // 
+            this.C3.BackColor = System.Drawing.Color.RoyalBlue;
+            resources.ApplyResources(this.C3, "C3");
+            this.C3.Name = "C3";
+            this.C3.UseMnemonic = false;
+            this.C3.UseVisualStyleBackColor = false;
+            this.C3.Click += new System.EventHandler(this.SelectTile);
+            // 
+            // playerBox
+            // 
+            this.playerBox.Controls.Add(this.OLabel);
+            this.playerBox.Controls.Add(this.XLabel);
+            this.playerBox.Controls.Add(this.isLabel2);
+            this.playerBox.Controls.Add(this.isLabel1);
+            this.playerBox.Controls.Add(this.player2NameLabel);
+            this.playerBox.Controls.Add(this.player1NameLabel);
+            this.playerBox.Controls.Add(this.player2Name);
+            this.playerBox.Controls.Add(this.player1Name);
+            resources.ApplyResources(this.playerBox, "playerBox");
+            this.playerBox.Name = "playerBox";
+            this.playerBox.TabStop = false;
+            // 
+            // OLabel
+            // 
+            resources.ApplyResources(this.OLabel, "OLabel");
+            this.OLabel.Name = "OLabel";
+            // 
+            // XLabel
+            // 
+            resources.ApplyResources(this.XLabel, "XLabel");
+            this.XLabel.Name = "XLabel";
+            // 
+            // isLabel2
+            // 
+            resources.ApplyResources(this.isLabel2, "isLabel2");
+            this.isLabel2.Name = "isLabel2";
+            // 
+            // isLabel1
+            // 
+            resources.ApplyResources(this.isLabel1, "isLabel1");
+            this.isLabel1.Name = "isLabel1";
+            // 
+            // player2NameLabel
+            // 
+            resources.ApplyResources(this.player2NameLabel, "player2NameLabel");
+            this.player2NameLabel.Name = "player2NameLabel";
+            // 
+            // player1NameLabel
+            // 
+            resources.ApplyResources(this.player1NameLabel, "player1NameLabel");
+            this.player1NameLabel.Name = "player1NameLabel";
+            // 
+            // player2Name
+            // 
+            resources.ApplyResources(this.player2Name, "player2Name");
+            this.player2Name.Name = "player2Name";
+            // 
+            // player1Name
+            // 
+            resources.ApplyResources(this.player1Name, "player1Name");
+            this.player1Name.Name = "player1Name";
+            // 
+            // playButton
+            // 
+            this.playButton.BackColor = System.Drawing.Color.HotPink;
+            resources.ApplyResources(this.playButton, "playButton");
+            this.playButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.playButton.Name = "playButton";
+            this.playButton.UseVisualStyleBackColor = false;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // Game
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.playerBox);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.A1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Game";
+            this.playerBox.ResumeLayout(false);
+            this.playerBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.GroupBox playerBox;
+        private System.Windows.Forms.Label OLabel;
+        private System.Windows.Forms.Label XLabel;
+        private System.Windows.Forms.Label isLabel2;
+        private System.Windows.Forms.Label isLabel1;
+        private System.Windows.Forms.Label player2NameLabel;
+        private System.Windows.Forms.Label player1NameLabel;
+        private System.Windows.Forms.TextBox player2Name;
+        private System.Windows.Forms.TextBox player1Name;
+        private System.Windows.Forms.Button playButton;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Windows.Forms;
+using Button = System.Windows.Forms.Button;
+
+namespace TicTacToe
+{
+    public partial class Game : Form
+    {
+        private string PlayerX;
+        private string PlayerO;
+        private int TurnNumber;
+
+        public Game()
+        {
+            //creates a new game board with all tiles starting as disabled.
+            InitializeComponent();
+            A1.Enabled = false;
+            A2.Enabled = false;
+            A3.Enabled = false;
+            B1.Enabled = false;
+            B2.Enabled = false;
+            B3.Enabled = false;
+            C1.Enabled = false;
+            C2.Enabled = false;
+            C3.Enabled = false;
+
+        }
+
+        protected void playButton_Click(object sender, EventArgs e)
+        {   //if the button text is play it checks to see if the use has inputted names
+            if (playButton.Text == "Play")
+            {
+                if (player1Name.Text == "")
+                {
+                    MessageBox.Show("Please enter the names of the players");
+                }
+                else if (player2Name.Text == "")
+                {
+                    MessageBox.Show("Please enter the names of the players");
+                }
+                //the user is prompted for names if one or both of the name spaces are blank
+                else
+                {
+                    //if names are inputted a welcome message is shown.
+                    PlayerX = player1Name.Text;
+                    PlayerO = player2Name.Text;
+
+                    MessageBox.Show("Welcome to Tic-Tac-Toe");
+                    playButton.Text = "Quit";
+
+                    TurnNumber = 1;
+                }
+                RefreshGrid();  //the game board is now ready to play
+            }
+            else
+            {
+               //at the end of the game the user is asked if they wish to change names
+                var dialogResult = MessageBox.Show("Do you want to change the players names?", "New Game?", MessageBoxButtons.YesNo);
+
+                if (dialogResult == DialogResult.Yes) //if "Yes" then a fresh game is made
+                {
+                    RefreshGrid();
+                    player1Name.Text = "";
+                    player2Name.Text = "";
+                }
+
+                playButton.Text = "Play";
+            }  
+        }
+
+        private void RefreshGrid() //makes a ready to play game board
+        {
+            A1.Enabled = true;
+            A2.Enabled = true;
+            A3.Enabled = true;
+            B1.Enabled = true;
+            B2.Enabled = true;
+            B3.Enabled = true;
+            C1.Enabled = true;
+            C2.Enabled = true;
+            C3.Enabled = true;
+
+            A1.Text = "";
+            A2.Text = "";
+            A3.Text = "";
+            B1.Text = "";
+            B2.Text = "";
+            B3.Text = "";
+            C1.Text = "";
+            C2.Text = "";
+            C3.Text = "";
+
+            A1.BackColor = System.Drawing.Color.RoyalBlue;
+            A2.BackColor = System.Drawing.Color.RoyalBlue;
+            A3.BackColor = System.Drawing.Color.RoyalBlue;
+            B1.BackColor = System.Drawing.Color.RoyalBlue;
+            B2.BackColor = System.Drawing.Color.RoyalBlue;
+            B3.BackColor = System.Drawing.Color.RoyalBlue;
+            C1.BackColor = System.Drawing.Color.RoyalBlue;
+            C2.BackColor = System.Drawing.Color.RoyalBlue;
+            C3.BackColor = System.Drawing.Color.RoyalBlue;
+        }
+
+        private void SelectTile(object sender, EventArgs e)
+        {
+            var tile = sender as Button;
+            if (TurnNumber % 2 == 0) //if its an even turn number it is O's turn
+            {
+                tile.Text = "O";
+            }
+            else
+            {
+                tile.Text = "X";
+            }
+
+            tile.Enabled = false; //the tile then becomes disabled so it cannot be changed
+            tile.BackColor = System.Drawing.Color.Pink;
+            TurnNumber++;
+            var matchFound = CheckForMatch();
+
+            if (!matchFound && TurnNumber == 10)
+            {
+                MessageBox.Show("The game is a draw!"); 
+                //if all tiles has been played without a match, it declares a draw
+            }
+            else if(matchFound)
+            {
+                DisableBoard();
+            }
+
+        }
+        private bool CheckForMatch()
+        {
+            //horizontal checks for an X match
+            if (A1.Text == "X" && B1.Text == "X" && C1.Text == "X" ||
+                A2.Text == "X" && B2.Text == "X" && C2.Text == "X" ||
+                A3.Text == "X" && B3.Text == "X" && C3.Text == "X")
+            {
+                MessageBox.Show($"{player1Name.Text} is the winner!");
+                return true;
+            }
+            //vertical checks for an X match 
+            if (A1.Text == "X" && A2.Text == "X" && A3.Text == "X" ||
+                B1.Text == "X" && B2.Text == "X" && B3.Text == "X" ||
+                C1.Text == "X" && C2.Text == "X" && C3.Text == "X")
+            {
+                MessageBox.Show($"{player1Name.Text} is the winner!");
+                return true;
+            }
+            //diagonal check for an X match
+            if (A1.Text == "X" && B2.Text == "X" && C3.Text == "X" ||
+                A3.Text == "X" && B2.Text == "X" && C1.Text == "X")
+            {
+                MessageBox.Show($"{player1Name.Text} is the winner!");
+                return true;
+            }
+
+            //horizontal checks for an O match
+            if (A1.Text == "O" && B1.Text == "O" && C1.Text == "O" ||
+                A2.Text == "O" && B2.Text == "O" && C2.Text == "O" ||
+                A3.Text == "O" && B3.Text == "O" && C3.Text == "O")
+            {
+                MessageBox.Show($"{player2Name.Text} is the winner!");
+                return true;
+            }
+            //vertical checks for an O match 
+            if (A1.Text == "O" && A2.Text == "O" && A3.Text == "O" ||
+                B1.Text == "O" && B2.Text == "O" && B3.Text == "O" ||
+                C1.Text == "O" && C2.Text == "O" && C3.Text == "O")
+            {
+                MessageBox.Show($"{player2Name.Text} is the winner!");
+                return true;
+            }
+            //diagonal check for an O match
+            if (A1.Text == "O" && B2.Text == "O" && C3.Text == "O" ||
+                A3.Text == "O" && B2.Text == "O" && C1.Text == "O")
+            {
+                MessageBox.Show($"{player2Name.Text} is the winner!");
+                return true;
+            }
+            return false;
+        }
+        private void DisableBoard()
+        {
+            A1.Enabled = false;
+            A2.Enabled = false;
+            A3.Enabled = false;
+            B1.Enabled = false;
+            B2.Enabled = false;
+            B3.Enabled = false;
+            C1.Enabled = false;
+            C2.Enabled = false;
+            C3.Enabled = false;
+        }
+    }
+}
